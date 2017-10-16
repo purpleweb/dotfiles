493ae6ddeb495e7795372a29e0847fba910c23df
Antoine Czerwonka - Purpleweb.fr <tonio.cz@gmail.com>
2017-10-16 21:16:21 +0200

    install ccolordiff

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 # install ag / The Silver Searcher
 # @see https://github.com/ggreer/the_silver_searcher
 apt-get install silversearcher-ag
+
+# install colordiff
+# @see https://www.colordiff.org/
+apt-get install colordiff
