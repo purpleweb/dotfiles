4ca2415c7334a7360ef91af2ecac156b2ff846f9
Antoine Czerwonka <antoine.czerwonka@infopro-digital.com>
2020-11-03 10:46:13 +0100

    git release clean

--- git-release.sh
+++ git-release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 git log master..develop | \
-    grep Merge | \
-    grep "into 'develop'" | \
+    grep -e "Merge .* into 'develop'" | \
     sed "s/' into 'develop'//" | \
     sed "s/.* Merge branch '//" | \
     sed 's/-/ /g' | \
+    sed 's/_/ /g' | \
     sed 's/feature //' | \
-    uniq | \
     sed 's/bugfix/FIX/' | \
     sed 's/hotfix/HOTFIX/' | \
-    sed 's/_/ /g' | \
+    sed 's/ *$//g' | \
+    uniq -ui | \
     sed 's/^/* /'
 
